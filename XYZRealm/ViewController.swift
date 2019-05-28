b17c1be8f0474040f1b53eca0a163369a71ce674
Brando <zhangqianbrandy2012@gmail.com>
2019-05-28 20:25:21 +0800

    Initial Commit

---
+++ XYZRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XYZRealm
+//
+//  Created by 张子豪 on 2019/5/28.
+//  Copyright © 2019 张子豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
